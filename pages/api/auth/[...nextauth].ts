6db24d98aefc62fea41d76476c6950fddf20d436
Seth McCullough <seth@endevrs.dev>
2022-11-20 08:15:07 -0500

    Move nextauth options to lib

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,13 +1,4 @@
 import NextAuth from "next-auth";
-import GoogleProvider from "next-auth/providers/github";
-
-export const authOptions = {
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID!,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-    }),
-  ],
-};
+import { authOptions } from "lib/nextauth";
 
 export default NextAuth(authOptions);
