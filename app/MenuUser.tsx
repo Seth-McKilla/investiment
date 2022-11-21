f4838267b0d251256119d021a31628ea3bc6561c
Seth McCullough <seth@endevrs.dev>
2022-11-20 20:02:18 -0500

    Add loading state for user menu

--- app/MenuUser.tsx
+++ app/MenuUser.tsx
@@ -10,53 +10,65 @@ import Button from "./components/Button";
 import { classNames } from "utils/styles";
 
 export default function MenuUser() {
-  const { data: session } = useSession();
+  const { data: session, status } = useSession();
 
-  return session?.user ? (
-    <Menu as="div" className="relative ml-3">
-      <Menu.Button className="flex items-center justify-center w-12 h-12 text-xl bg-gray-200 rounded-full focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
-        <Avatar />
-      </Menu.Button>
-      <Transition
-        as={Fragment}
-        enter="transition ease-out duration-100"
-        enterFrom="transform opacity-0 scale-95"
-        enterTo="transform opacity-100 scale-100"
-        leave="transition ease-in duration-75"
-        leaveFrom="transform opacity-100 scale-100"
-        leaveTo="transform opacity-0 scale-95"
-      >
-        <Menu.Items className="absolute right-0 z-10 w-48 pt-1 mt-2 origin-top-right bg-white rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-          <Menu.Item>
-            {({ active }) => (
-              <Link
-                href="/profile"
-                className={classNames(
-                  active ? "bg-gray-100" : "",
-                  "block px-4 py-3 text-sm text-gray-700"
-                )}
-              >
-                Profile
-              </Link>
-            )}
-          </Menu.Item>
-          <Menu.Item>
-            {({ active }) => (
-              <Menu.Button
-                className={classNames(
-                  active ? "bg-gray-100" : "",
-                  "block px-4 py-2 text-sm w-full bg-indigo-600 text-white rounded-b-md hover:bg-indigo-700"
-                )}
-                onClick={() => signOut()}
-              >
-                Sign out
-              </Menu.Button>
-            )}
-          </Menu.Item>
-        </Menu.Items>
-      </Transition>
-    </Menu>
-  ) : (
+  if (status === "loading") {
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <div className="w-12 h-12 bg-white rounded-full animate-pulse" />
+      </div>
+    );
+  }
+
+  if (status === "authenticated" && session?.user) {
+    return (
+      <Menu as="div" className="relative ml-3">
+        <Menu.Button className="flex items-center justify-center w-12 h-12 text-xl bg-gray-200 rounded-full focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+          <Avatar />
+        </Menu.Button>
+        <Transition
+          as={Fragment}
+          enter="transition ease-out duration-100"
+          enterFrom="transform opacity-0 scale-95"
+          enterTo="transform opacity-100 scale-100"
+          leave="transition ease-in duration-75"
+          leaveFrom="transform opacity-100 scale-100"
+          leaveTo="transform opacity-0 scale-95"
+        >
+          <Menu.Items className="absolute right-0 z-10 w-48 pt-1 mt-2 origin-top-right bg-white rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+            <Menu.Item>
+              {({ active }) => (
+                <Link
+                  href="/profile"
+                  className={classNames(
+                    active ? "bg-gray-100" : "",
+                    "block px-4 py-3 text-sm text-gray-700"
+                  )}
+                >
+                  Profile
+                </Link>
+              )}
+            </Menu.Item>
+            <Menu.Item>
+              {({ active }) => (
+                <Menu.Button
+                  className={classNames(
+                    active ? "bg-gray-100" : "",
+                    "block px-4 py-2 text-sm w-full bg-indigo-600 text-white rounded-b-md hover:bg-indigo-700"
+                  )}
+                  onClick={() => signOut()}
+                >
+                  Sign out
+                </Menu.Button>
+              )}
+            </Menu.Item>
+          </Menu.Items>
+        </Transition>
+      </Menu>
+    );
+  }
+
+  return (
     <Link href="/signin">
       <Button>Sign in</Button>
     </Link>
--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,9 @@ export default function HomePage() {
   return (
     <div className="flex flex-col items-center justify-center h-screen -mt-20">
       <h1 className="mb-4 text-3xl">Welcome to Investiment!</h1>
-      <p className="mb-4 text-4xl font-bold h1">
-        Sentiment analyses of the masses for your favorite stock picks
+      <p className="mb-4 text-4xl font-bold text-center h1">
+        Effortlessly keep track of market sentiment for your favorite
+        securities.
       </p>
       <Link href="/signin">
         <Button>Get started</Button>
